6affa3d757e75f0994f0ea8a947b24e1ddf09d43
kunuwake3 <kunuwake3@gmail.com>
2025-06-06 19:28:34 +0800

    Update main_m1.h

--- main_m1.h
+++ main_m1.h
@@ -3,9 +3,9 @@
 
 #include <QObject>
 #include <QSqlDatabase>
-#include <QSqlQuery>
 #include <QString>
 #include <QVariantList>
+#include "simplecrypt.h"
 
 class Main_M1 : public QObject
 {
@@ -13,21 +13,18 @@ class Main_M1 : public QObject
 public:
     explicit Main_M1(QObject *parent = nullptr);
 
-    // Открытие шифрованной базы (portable, SQLCipher)
     bool openDatabase(const QString &dbPath, const QString &masterPassword);
 
-    // Проверка, открыта ли БД
     bool isOpen() const;
 
-    // Работа с серверами (пример)
     QVariantList getAllServers();
     bool addServer(const QVariantList &fields);
     bool deleteServer(int id);
 
-    // и др. методы по необходимости
-
 private:
     QSqlDatabase db;
+    SimpleCrypt *crypt;
+    quint64 deriveKey(const QString &password);
 };
 
 #endif // MAIN_M1_H
